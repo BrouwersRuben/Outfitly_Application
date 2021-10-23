456f8f1373237a74fd2bb36a1aa36cda40807024
Dominik Mika <dominik.mika@student.kdg.be>
2021-10-23 14:34:55 +0200

    Added component annotation.

--- src/main/java/be/kdg/outfitly/repository/MainUserListRepository.java
+++ src/main/java/be/kdg/outfitly/repository/MainUserListRepository.java
@@ -1,7 +1,9 @@
 package be.kdg.outfitly.repository;
 
 import be.kdg.outfitly.domain.MainUser;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MainUserListRepository  extends ListRepository<MainUser> {
 
 }
