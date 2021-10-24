2763487e596063745820bb25a3e394bf767c6fda
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2021-10-24 19:45:38 +0200

    thymeleaf stuff, not working mainpage

--- src/main/java/be/kdg/outfitly/domain/User.java
+++ src/main/java/be/kdg/outfitly/domain/User.java
@@ -3,17 +3,39 @@ package be.kdg.outfitly.domain;
 import java.util.List;
 
 public class User extends Entity {
-    //login details
-    protected String email;
-    protected String password;
 
-    protected String name;
-    protected List<ClothingItem> clothes;
+    // Variables
+
+    private final String email;
+    private final String password;
+
+    private final String name;
+    private final List<ClothingItem> clothes;
+
+    // Constructor
 
     public User(String email, String password, String name, List<ClothingItem> clothes) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.clothes = clothes;
+    }
+
+    // Getters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ClothingItem> getClothes() {
+        return clothes;
     }
 }
