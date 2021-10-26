98450c4d30c5e0a9e982d01ead34cbd635a23811
Dominik Mika <dominik.mika@student.kdg.be>
2021-10-26 10:49:08 +0200

    Added a method to return a map of clothes by types.

--- src/main/java/be/kdg/outfitly/domain/OutfitSelector.java
+++ src/main/java/be/kdg/outfitly/domain/OutfitSelector.java
@@ -1,12 +1,26 @@
 package be.kdg.outfitly.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutfitSelector {
     private WeatherForecast weatherForecast;
     private User user;
+    private ClothingItem.Occasion occasion;
 
+    public OutfitSelector(WeatherForecast weatherForecast, User user, ClothingItem.Occasion occasion) {
+        this.weatherForecast = weatherForecast;
+        this.user = user;
+        this.occasion = occasion;
+    }
+
+    private Logger logger = LoggerFactory.getLogger(OutfitSelector.class);
 
 
     public List<ClothingItem> getPossibleClothingItems() {
@@ -42,8 +56,8 @@ public class OutfitSelector {
 
 
         if (temperature < 5) {
-           givenWeather = ClothingItem.Weather.COLD;
-        } else if(temperature < 15){
+            givenWeather = ClothingItem.Weather.COLD;
+        } else if (temperature < 15) {
             givenWeather = ClothingItem.Weather.MILD;
         } else {
             givenWeather = ClothingItem.Weather.WARM;
@@ -55,13 +69,38 @@ public class OutfitSelector {
                 .collect(Collectors.toList());
     }
 
-    public List<ClothingItem> removeUnsuitableForOccasion(List<ClothingItem> clothes, ClothingItem.Occasion occasion){
+    public List<ClothingItem> removeUnsuitableForOccasion(List<ClothingItem> clothes, ClothingItem.Occasion occasion) {
 
         return clothes
                 .stream()
                 .filter(item -> item.getOccasion() == occasion || item.getOccasion() == ClothingItem.Occasion.UNIVERSAL)
                 .collect(Collectors.toList());
 
+    }
+
+    public Map<ClothingItem.Type, List<ClothingItem>> divideClothesIntoTypes(List<ClothingItem> clothes) {
+
+        List<ClothingItem.Type> types = new ArrayList<>(List.of(ClothingItem.Type.values()));
+
+        Map<ClothingItem.Type, List<ClothingItem>> clothesByType = new HashMap<>();
+
+        types.stream()
+                .forEach(type -> {
+                    List<ClothingItem> clothesOfType = clothes
+                            .stream()
+                            .filter(item -> item.getType() == type)
+                            .collect(Collectors.toList());
+
+                    clothesByType.put(type, clothesOfType);
+                });
+
+        return clothesByType;
+    }
+
+    public Map<ClothingItem.Type, List<ClothingItem>> getSuitableClothesMap() {
+        Map<ClothingItem.Type, List<ClothingItem>> suitableClothes = divideClothesIntoTypes(getPossibleClothingItems());
+        logger.debug(suitableClothes.toString());
+        return suitableClothes;
     }
 
 }
