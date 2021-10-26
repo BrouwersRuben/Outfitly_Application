8a67bb91e9874fff4f76a2654b32068fb6e849ad
Dominik Mika <dominik.mika@student.kdg.be>
2021-10-26 14:40:23 +0200

    Changed occasion selector page to automatically use enum values with thymeleaf.

--- src/main/java/be/kdg/outfitly/presentation/OutfitController.java
+++ src/main/java/be/kdg/outfitly/presentation/OutfitController.java
@@ -3,21 +3,24 @@ package be.kdg.outfitly.presentation;
 import be.kdg.outfitly.domain.ClothingItem;
 import be.kdg.outfitly.domain.OutfitSelector;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/outfit")
 public class OutfitController {
 
     private OutfitSelector outfitSelector;
 
-    @GetMapping("/occasion")
-    public String occasionSelector(){
+    @GetMapping("/choose-occasion")
+    public String occasionSelector(Model model){
+        model.addAttribute("occasions", ClothingItem.Occasion.values());
         return "selectoccasion";
     }
 
-    @PostMapping("/occasion")
+    @PostMapping("/choose-occasion")
     public String occasionForm(ClothingItem.Occasion occasion){
 
         //TODO:
