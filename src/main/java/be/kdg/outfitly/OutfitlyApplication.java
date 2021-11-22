ef643cf8cee275334736396e4f81af5601a996c7
rubenbrouwers <KdGD89BbGw>
2021-11-22 21:36:13 +0100

    scheduling start

--- src/main/java/be/kdg/outfitly/OutfitlyApplication.java
+++ src/main/java/be/kdg/outfitly/OutfitlyApplication.java
@@ -2,8 +2,10 @@ package be.kdg.outfitly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OutfitlyApplication {
 
     public static void main(String[] args) {
