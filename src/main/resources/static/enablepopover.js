38f4471ad0a8e7cdca6f3163c460669424034842
rubenbrouwers <KdGD89BbGw>
2021-11-26 14:16:50 +0100

    popover fixed

---
+++ src/main/resources/static/enablepopover.js
@@ -0,0 +1,6 @@
+// Enable popovers
+console.log("Enabling popovers.")
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
