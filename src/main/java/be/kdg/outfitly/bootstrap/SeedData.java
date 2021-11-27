0bb73b729223cf34d70e1927df6a8609f8f76be7
Dominik Mika <dominik.mika@student.kdg.be>
2021-11-27 13:31:31 +0100

    Moved weather API calling method to WeatherForecast class. Added findByCountryAndCity method to WeatherForecastService.

--- src/main/java/be/kdg/outfitly/bootstrap/SeedData.java
+++ src/main/java/be/kdg/outfitly/bootstrap/SeedData.java
@@ -52,16 +52,15 @@ public class SeedData implements CommandLineRunner {
     public void run(String... args) throws Exception {
         logger.debug("Seeding the repositories");
         //TODO: Make this so that the location changes per user it loads.
-        weatherAPIData = retrieveAPIData(logger, "Antwerp,BE");
 //        arduinoAPIData = retrieveAPIData(logger, arduinoAPI);
 
         //Test users with some clothing items
-        User user1 = new User("testUser1@gmail.com","test123","John", "Doe", "0475441658", "Belgium", "Antwerp", "Nationale Straat", 85, "2000",
-                List.of(new ClothingItem("Jacket", ClothingItem.Material.LEATHER, ClothingItem.RainProofness.NORMAL,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.MILD, ClothingItem.Type.JACKET_LIKE),
-                        new ClothingItem("Hoodie", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.COLD, ClothingItem.Type.SWEATSHIRT_LIKE),
-                        new ClothingItem("Jeans", ClothingItem.Material.DENIM, ClothingItem.RainProofness.GOOD,ClothingItem.Occasion.UNIVERSAL,ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.TROUSERS_LIKE),
-                        new ClothingItem("T-Shirt", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.WARM, ClothingItem.Type.T_SHIRT_LIKE),
-                        new ClothingItem("Sport shoes", ClothingItem.Material.OTHER, ClothingItem.RainProofness.BAD,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.SHOES),
+        User user1 = new User("testUser1@gmail.com", "test123", "John", "Doe", "0475441658", "Belgium", "Antwerp", "Nationale Straat", 85, "2000",
+                List.of(new ClothingItem("Jacket", ClothingItem.Material.LEATHER, ClothingItem.RainProofness.NORMAL, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.MILD, ClothingItem.Type.JACKET_LIKE),
+                        new ClothingItem("Hoodie", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.COLD, ClothingItem.Type.SWEATSHIRT_LIKE),
+                        new ClothingItem("Jeans", ClothingItem.Material.DENIM, ClothingItem.RainProofness.GOOD, ClothingItem.Occasion.UNIVERSAL, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.TROUSERS_LIKE),
+                        new ClothingItem("T-Shirt", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.WARM, ClothingItem.Type.T_SHIRT_LIKE),
+                        new ClothingItem("Sport shoes", ClothingItem.Material.OTHER, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.SHOES),
                         new ClothingItem("Leather dress shoes", ClothingItem.Material.LEATHER, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.ELEGANT, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.SHOES),
                         new ClothingItem("White dress shirt", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.ELEGANT, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.SHIRT_LIKE),
                         new ClothingItem("Blue blazer", ClothingItem.Material.WOOL, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.ELEGANT, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.JACKET_LIKE),
@@ -71,14 +70,14 @@ public class SeedData implements CommandLineRunner {
                         new ClothingItem("White polo shirt", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.UNIVERSAL, ClothingItem.Weather.WARM, ClothingItem.Type.T_SHIRT_LIKE),
                         new ClothingItem("Black pants", ClothingItem.Material.OTHER, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.UNIVERSAL, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.TROUSERS_LIKE),
                         new ClothingItem("Black leather sneakers", ClothingItem.Material.LEATHER, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.UNIVERSAL, ClothingItem.Weather.UNIVERSAL, ClothingItem.Type.SHOES)
-        ));
+                ));
 
-        User user2 = new User("testUser2@gmail.com","test123","Bob","Shannon","0458564572", "Spain", "Barcelona", "La Rambla", 15, "08001",
-                List.of(new ClothingItem("Jacket", ClothingItem.Material.LEATHER, ClothingItem.RainProofness.NORMAL,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.MILD),
-                        new ClothingItem("Hoodie", ClothingItem.Material.SYNTHETIC, ClothingItem.RainProofness.NORMAL,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.COLD),
-                        new ClothingItem("Jeans", ClothingItem.Material.DENIM, ClothingItem.RainProofness.GOOD,ClothingItem.Occasion.UNIVERSAL,ClothingItem.Weather.UNIVERSAL),
-                        new ClothingItem("T-Shirt", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD,ClothingItem.Occasion.CASUAL,ClothingItem.Weather.WARM),
-                        new ClothingItem("Suit", ClothingItem.Material.SILK, ClothingItem.RainProofness.BAD,ClothingItem.Occasion.ELEGANT,ClothingItem.Weather.UNIVERSAL)));
+        User user2 = new User("testUser2@gmail.com", "test123", "Bob", "Shannon", "0458564572", "Spain", "Barcelona", "La Rambla", 15, "08001",
+                List.of(new ClothingItem("Jacket", ClothingItem.Material.LEATHER, ClothingItem.RainProofness.NORMAL, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.MILD),
+                        new ClothingItem("Hoodie", ClothingItem.Material.SYNTHETIC, ClothingItem.RainProofness.NORMAL, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.COLD),
+                        new ClothingItem("Jeans", ClothingItem.Material.DENIM, ClothingItem.RainProofness.GOOD, ClothingItem.Occasion.UNIVERSAL, ClothingItem.Weather.UNIVERSAL),
+                        new ClothingItem("T-Shirt", ClothingItem.Material.COTTON, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.CASUAL, ClothingItem.Weather.WARM),
+                        new ClothingItem("Suit", ClothingItem.Material.SILK, ClothingItem.RainProofness.BAD, ClothingItem.Occasion.ELEGANT, ClothingItem.Weather.UNIVERSAL)));
 
         userRepository.create(user1);
         userRepository.create(user2);
@@ -93,51 +92,24 @@ public class SeedData implements CommandLineRunner {
         arduinoSensorRepository.create(arduinoSensor);
 
         // Weather API
-        WeatherForecast forecast = new WeatherForecast(
-                LocalDateTime.of(2021, 10, 29, 12, 30, 30),
-                String.valueOf(weatherAPIData.get("name")),
-                String.valueOf(weatherAPIData.getJSONObject("sys").get("country")),
-                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp"))),
-                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("feels_like"))),
-                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp_min"))),
-                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp_max"))),
-                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("wind").get("speed"))),
-                Integer.parseInt(String.valueOf(weatherAPIData.getJSONObject("main").get("humidity"))),
-                String.valueOf(weatherAPIData.getJSONArray("weather").getJSONObject(0).get("main"))
-        );
+//        WeatherForecast forecast = new WeatherForecast(
+//                LocalDateTime.of(2021, 10, 29, 12, 30, 30),
+//                String.valueOf(weatherAPIData.get("name")),
+//                String.valueOf(weatherAPIData.getJSONObject("sys").get("country")),
+//                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp"))),
+//                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("feels_like"))),
+//                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp_min"))),
+//                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp_max"))),
+//                Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("wind").get("speed"))),
+//                Integer.parseInt(String.valueOf(weatherAPIData.getJSONObject("main").get("humidity"))),
+//                String.valueOf(weatherAPIData.getJSONArray("weather").getJSONObject(0).get("main"))
+//        );
 
-        weatherForecastRepository.create(forecast);
+        WeatherForecast forecast = WeatherForecast.currentForecastForCountryCity("Belgium", "Antwerp");
+        if (forecast != null) weatherForecastRepository.create(forecast);
 
         // Slave and master accounts may be a bit easier to understand
     }
 
-    private JSONObject retrieveAPIData(Logger logger, String location) throws Exception{
-        String APILink = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&appid=ff81fe37ad2b546130b7cbcb331aa72c";
-        // TODO: Reload api on refresh
-        URIBuilder builder = new URIBuilder(APILink);
-        HttpGet get = new HttpGet(builder.build());
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        JSONObject json;
 
-        try (CloseableHttpResponse response = httpclient.execute(get)) {
-            json = null;
-            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-                logger.error("Bad response status code: {}.", response.getStatusLine().getStatusCode());
-//                return;
-            }
-
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                String rawResult = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-                json = new JSONObject(rawResult);
-            }
-
-            if (json ==null) {
-                logger.error("No weather forecast data retrieved!");
-            } else {
-                logger.debug("Forecast data successfully retrieved!");
-            }
-        }
-        return json;
-    }
 }
--- src/main/java/be/kdg/outfitly/domain/WeatherForecast.java
+++ src/main/java/be/kdg/outfitly/domain/WeatherForecast.java
@@ -1,5 +1,18 @@
 package be.kdg.outfitly.domain;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 // TODO: This is only for the CURRENT weather forecast, we still need a separate class for future forecast I guess.
@@ -15,6 +28,8 @@ public class WeatherForecast extends Entity {
     private double windSpeed;
     private int humidity;
     private String weatherDescription;
+
+    private static final Logger logger = LoggerFactory.getLogger(WeatherForecast.class);
 
     public WeatherForecast(LocalDateTime date, String city, String country, double temperature, double feelsLikeTemperature, double lowestTemperature, double highestTemperature, double windSpeed, int humidity, String weatherDescription) {
         this.date = date;
@@ -32,11 +47,60 @@ public class WeatherForecast extends Entity {
     public WeatherForecast() {
     }
 
-    public static WeatherForecast randomForecast(){
+    public static WeatherForecast currentForecastForCountryCity(String country, String city){
         WeatherForecast wf = new WeatherForecast();
-        wf.lowestTemperature = 16;
-        wf.weatherDescription = "";
+        JSONObject weatherAPIData;
+        try {
+            weatherAPIData = retrieveAPIData(city + "," + country);
+        }catch (Exception e){
+            logger.error("An error occurred while data was being retrieved from the API.");
+            return null;
+        }
+
+        wf.setDate(LocalDateTime.now());
+        wf.setCountry(country);
+        wf.setCity(city);
+        wf.setTemperature(Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp"))));
+        wf.setFeelsLikeTemperature(Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("feels_like"))));
+        wf.setLowestTemperature(Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp_min"))));
+        wf.setHighestTemperature(Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("main").get("temp_max"))));
+        wf.setWindSpeed(Double.parseDouble(String.valueOf(weatherAPIData.getJSONObject("wind").get("speed"))));
+        wf.setHumidity(Integer.parseInt(String.valueOf(weatherAPIData.getJSONObject("main").get("humidity"))));
+        wf.setWeatherDescription(String.valueOf(weatherAPIData.getJSONArray("weather").getJSONObject(0).get("main")));
+
+        logger.debug("Weather forecast object created: ");
+
         return wf;
+    }
+
+    private static JSONObject retrieveAPIData(String location) throws Exception{
+        String APILink = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=metric&appid=ff81fe37ad2b546130b7cbcb331aa72c";
+        // TODO: Reload api on refresh
+        URIBuilder builder = new URIBuilder(APILink);
+        HttpGet get = new HttpGet(builder.build());
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        JSONObject json;
+
+        try (CloseableHttpResponse response = httpclient.execute(get)) {
+            json = null;
+            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                logger.error("Bad response status code: {}.", response.getStatusLine().getStatusCode());
+//                return;
+            }
+
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                String rawResult = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+                json = new JSONObject(rawResult);
+            }
+
+            if (json ==null) {
+                logger.error("No weather forecast data retrieved!");
+            } else {
+                logger.debug("Forecast data successfully retrieved!");
+            }
+        }
+        return json;
     }
 
     // Note: I can retrieve any info from the API, though retrieving some data might be tricky due to the fact that we're using a trial version.
@@ -82,6 +146,50 @@ public class WeatherForecast extends Entity {
 
     public String getWeatherDescription() {
         return weatherDescription;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setFeelsLikeTemperature(double feelsLikeTemperature) {
+        this.feelsLikeTemperature = feelsLikeTemperature;
+    }
+
+    public void setLowestTemperature(double lowestTemperature) {
+        this.lowestTemperature = lowestTemperature;
+    }
+
+    public void setHighestTemperature(double highestTemperature) {
+        this.highestTemperature = highestTemperature;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
     }
 
     // https://openweathermap.org/weather-conditions
