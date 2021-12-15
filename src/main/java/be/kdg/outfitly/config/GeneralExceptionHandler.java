a9867a6adaca15753bbba8591d26c3f7cd162297
rubenbrouwers <KdGD89BbGw>
2021-12-15 23:56:28 +0100

    general error page

---
+++ src/main/java/be/kdg/outfitly/config/GeneralExceptionHandler.java
@@ -0,0 +1,33 @@
+package be.kdg.outfitly.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@ControllerAdvice
+public class GeneralExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(GeneralExceptionHandler.class);
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "General exception occured...")
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception exception) throws Exception{
+        if (AnnotationUtils.findAnnotation (exception.getClass(), ResponseStatus.class) != null) {
+            throw exception;
+        }
+        log.error("Exception: " + exception.getMessage());
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", exception);
+        mav.addObject("url", req.getRequestURL());
+        mav.addObject("timestamp", new Date().toString());
+        mav.setViewName("error");
+        return mav;
+    }
+}
